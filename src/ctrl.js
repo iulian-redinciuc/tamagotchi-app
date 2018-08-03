0702685df737be1d843cd583c9cffbdb0f09602e
Radu Luca <radu.luca13@yahoo.com>
2018-08-03 14:31:39 +0300

    summary updated

---
+++ src/ctrl.js
@@ -0,0 +1,10 @@
+let submitBtn = document.getElementById('sbm-form');
+submitBtn.addEventListener('click', function(e) {
+  e.preventDefault();
+  let petNameInput = document.getElementById('pet-name');
+  let petName = petNameInput.value;
+
+  let view = new createPETDIV(PetModel, petName);
+  PetModel.addPet(petName);
+  petNameInput.value = '';
+});
